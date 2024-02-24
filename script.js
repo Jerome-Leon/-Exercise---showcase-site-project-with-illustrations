7020a0800019ddcf34ff0bb9e2447dc140811be1
Jerome Leon <jerome_leon@yahoo.fr>
2024-02-24 17:28:00 +0100

    Retouche javascript du menu

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const links = document.querySelectorAll('nav li');
+const links = document.querySelectorAll("nav li");
 
 icons.addEventListener("click", () => {
     nav.classList.toggle("active");
